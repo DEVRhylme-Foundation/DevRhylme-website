5599cfda3da781cd122c16fcf1ad35d86ef14bd4
Rishi Mondal <mavrickrishi@gmail.com>
2024-10-14 08:06:33 +0530

    bug removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "coming soon",
+  site: "https://fuwari.vercel.app/",
   base: "/",
   trailingSlash: "always",
   integrations: [
